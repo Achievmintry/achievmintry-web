ce0bec0acaf8f596c7a197e8733632a69cd9f52c
luxumbra <me@luxumbra.dev>
2020-11-26 07:30:29 +0000

    Tweaks to Home and Communities pages

--- packages/react-app/src/views/Community.js
+++ packages/react-app/src/views/Community.js
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { Image, Box, Heading, Text, Grid } from "@chakra-ui/core";
+import {
+  Image,
+  Box,
+  Heading,
+  Text,
+  SimpleGrid,
+  Flex,
+  AspectRatioBox
+} from "@chakra-ui/core";
 import { useHistory, useParams } from "react-router-dom";
 import { useCommunityApi } from "../contexts/DappContext";
-import { Chievs } from "../components";
+import { Chievs, Balance } from "../components";
 
 const Community = ({ dao }) => {
   const [communities] = useCommunityApi();
@@ -13,8 +21,8 @@ const Community = ({ dao }) => {
 
   useEffect(() => {
     const _dao = communities
-      .map((item) => item.fields)
-      .find((item) => item["Dao Address"] === currentDao);
+      .map(item => item.fields)
+      .find(item => item["Dao Address"] === currentDao);
     setDaoData(_dao);
   }, [communities, currentDao]);
 
@@ -28,29 +36,38 @@ const Community = ({ dao }) => {
     // eslint-disable-next-line
   }, [addr]);
   return (
-    <>
-      <Grid>
+    <Box bg="brandYellow.200" w="100%" minH="100vh" textAlign="center" pt="10">
+      <Box mx="auto" maxW="90vw" textAlign="left">
         {daoData && (
-          <Box
-            maxW="18rem"
-            borderWidth="1px"
-            rounded="lg"
+          <Flex
+            align="flex-start"
             overflow="hidden"
-            borderColor="brandPink.900"
-            p="6"
+            direction="row"
+            wrap="nowrap"
           >
-            <Image src={daoData.Logo[0].url} />
-            <Box p="6">
-              <Heading as="h3" size="lg">
+            <Box p="6" w="300px" flexShrink="0">
+              <AspectRatioBox
+                maxW="300px"
+                ratio={1}
+                borderBottom="10px solid black"
+              >
+                <Image src={daoData.Logo[0].url} />
+              </AspectRatioBox>
+            </Box>
+            <Box p="6" flexGrow="1">
+              <Heading as="h2" size="2xl">
                 {daoData.Name}
               </Heading>
               <Text>{daoData.Blurb}</Text>
             </Box>
-          </Box>
+          </Flex>
         )}
-      </Grid>
-      {daoData?.Name && <Chievs dao={daoData.Name} />}
-    </>
+
+        {daoData?.Name && (
+          <Chievs dao={daoData.Name} cols={{ sm: 1, md: 2, lg: 4 }} />
+        )}
+      </Box>
+    </Box>
   );
 };
 
--- packages/react-app/src/views/Home.js
+++ packages/react-app/src/views/Home.js
@@ -1,26 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Flex, Box, Button, Heading, Text } from "@chakra-ui/core";
-import { Balance, Chievs, Communities } from "../components";
+import { Chievs, Communities } from "../components";
 
 const Home = () => {
   return (
-    <Flex bg="brandYellow.200" align="center" direction="column" wrap>
-      <Balance w="full" />
-      <Box>
+    <Box bg="brandYellow.200" w="100%" textAlign="center">
+      <Box mx="auto" maxW="90vw" textAlign="left">
         <Chievs featured={true} />
         <Box p="6">
           <Heading as="h2" mb="1" textTransform="uppercase">
             Communities
           </Heading>
-          <Text mb="3">Find your community specific talisman</Text>
+          <Text fontSize="2xl" mb="5">
+            Find your community specific talisman
+          </Text>
           <Communities featured={true} />
-          <Flex direction="row" wrap="nowrap" align="start" justify="start">
-            <Box p="6">
+          <Flex
+            direction="row"
+            wrap="nowrap"
+            align="start"
+            justify="start"
+            borderTop="10px"
+            borderColor="black"
+          >
+            <Box p="6" mb="5" w={{ sm: "auto", md: "33%" }}>
               <Heading as="h2" mb="1" textTransform="uppercase">
                 NFT Artists
               </Heading>
-              <Text mb="3">
+              <Text fontSize="2xl" mb="5">
                 Submit your work To own a Gen0 NFT talisman and get a % of all
                 sales.
               </Text>
@@ -28,11 +36,13 @@ const Home = () => {
                 Submissions
               </Button>
             </Box>
-            <Box p="6">
+            <Box p="6" w={{ sm: "auto", md: "33%" }}>
               <Heading as="h2" mb="1" textTransform="uppercase">
                 Bots
               </Heading>
-              <Text mb="3">Add the tip bot to your discord</Text>
+              <Text fontSize="2xl" mb="5">
+                Add the tip bot to your discord
+              </Text>
               <Button bg="transparent" border="1px">
                 Coming Soon
               </Button>
@@ -40,7 +50,7 @@ const Home = () => {
           </Flex>
         </Box>
       </Box>
-    </Flex>
+    </Box>
   );
 };
 
