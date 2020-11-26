534eb597b9d3ae859e6dedbb4f9bb3ae595dddb0
luxumbra <me@luxumbra.dev>
2020-11-26 06:24:55 +0000

    Add balance to UserAvatar - should probably create a user component to wrap these.

--- packages/react-app/src/components/UserAvatar.js
+++ packages/react-app/src/components/UserAvatar.js
@@ -1,27 +1,34 @@
-import React from 'react';
-import makeBlockie from 'ethereum-blockies-base64';
-import { Flex, Avatar } from '@chakra-ui/core';
-
-import { truncateAddr } from '../utils/Helpers';
+import React from "react";
+import makeBlockie from "ethereum-blockies-base64";
+import { Box, Flex, Avatar } from "@chakra-ui/core";
+import Balance from "./Balance";
+import { truncateAddr } from "../utils/Helpers";
 
 const UserAvatar = ({ user }) => {
   return (
-    <Flex direction='row' alignItems='center'>
-      {user && user.image && user.image[0] ? (
-        <Avatar
-          name={user.username}
-          src={`${'https://ipfs.infura.io/ipfs/' +
-            user.image[0].contentUrl['/']}`}
-          mr={3}
-        />
-      ) : (
-        <Avatar name={user.username} src={makeBlockie(user.username)} mr={3} />
-      )}
-      <h3>
-        {user.name || truncateAddr(user.username)}{' '}
-        <span>{user.emoji || ''} </span>
-      </h3>
-    </Flex>
+    <Box textAlign="right">
+      <Flex direction="row" alignItems="center" justifyContent="flex-end">
+        {user && user.image && user.image[0] ? (
+          <Avatar
+            name={user.username}
+            src={`${"https://ipfs.infura.io/ipfs/" +
+              user.image[0].contentUrl["/"]}`}
+            mr={3}
+          />
+        ) : (
+          <Avatar
+            name={user.username}
+            src={makeBlockie(user.username)}
+            mr={3}
+          />
+        )}
+        <h3>
+          {user.name || truncateAddr(user.username)}{" "}
+          <span>{user.emoji || ""} </span>
+        </h3>
+      </Flex>
+      <Balance w="auto" justifyContent="flex-end" />
+    </Box>
   );
 };
 
