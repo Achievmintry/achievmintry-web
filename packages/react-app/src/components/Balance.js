b1fc8864c590c9f1b74e2c9a33a267d3ed8d35f8
luxumbra <me@luxumbra.dev>
2020-11-26 07:28:38 +0000

    Rounding up the balances to conserve space.

--- packages/react-app/src/components/Balance.js
+++ packages/react-app/src/components/Balance.js
@@ -5,23 +5,29 @@ import { useUserWallet } from "../contexts/DappContext";
 
 const Balance = props => {
   const [userWallet] = useUserWallet();
+  console.log(userWallet);
+  const roundUp = balance => {
+    if (typeof balance !== "string") {
+      return Math.round((balance + Number.EPSILON) * 100) / 100;
+    }
+    return parseFloat(balance).toFixed(2);
+  };
+
   return userWallet ? (
     <Flex
       align="center"
-      justify="space-between"
+      justify="right"
       wrap="wrap"
-      padding="1.5rem"
-      bg="black"
-      color="white"
-      fontFamily="heading"
-      pos="sticky"
-      top={96}
-      boxShadow="lg"
-      zIndex={100}
+      padding="3px"
+      color="black"
       {...props}
     >
-      <Text>xDai: {userWallet?.eth}</Text>
-      <Text>$CHIEV: {userWallet?.chiev}</Text>
+      <Text fontFamily="heading" fontSize="xs" mr="3" isTruncated>
+        xDai: {roundUp(userWallet?.eth)}
+      </Text>
+      <Text fontFamily="heading" fontSize="xs" isTruncated>
+        $CHIEV: {roundUp(userWallet?.chiev)}
+      </Text>
     </Flex>
   ) : null;
 };
